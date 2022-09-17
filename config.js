3e61bbb952900af9e65297937ea67d7a60c2821d
sata11nn <112214968+sata11nn@users.noreply.github.com>
2022-09-18 01:41:45 +0430

    Update config.js
    Signed-off-by: sata11nn <112214968+sata11nn@users.noreply.github.com>

--- config.js
+++ config.js
@@ -15,14 +15,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['989339658384']
-global.premium = ['989339658384']
-global.botname = 'BALOCH EDIT'
-global.ownername = 'HOSEIN SARBAZI'
-global.group1 = 'https://chat.whatsapp.com/DtpQbVzujrfLRB2xvl97pp'
-global.group2 = 'https://chat.whatsapp.com/EWXqwpZO0GL45MP1ys3asy'
-global.packname = 'BALOCH EDIT'
-global.ytchannel = 'https://youtube.com/channel/UCIdvdgFwdssFC0Nxx9Xk1dQ'
+global.owner = ['989175142780']
+global.premium = ['989175142780']
+global.botname = 'SATA'
+global.ownername = 'SATA'
+global.group1 = 'https://chat.whatsapp.com/HWUyQZfZaIaEp7TN1rqLsx'
+global.group2 = 'https://chat.whatsapp.com/HWUyQZfZaIaEp7TN1rqLsx'
+global.packname = 'SATA'
+global.ytchannel = 'https://chat.whatsapp.com/HWUyQZfZaIaEp7TN1rqLsx'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
